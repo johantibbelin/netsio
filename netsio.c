ef712fddd2ad5acadeb6643aae6a784b8c7454a4
Johan Tibbelin <42235680+johantibbelin@users.noreply.github.com>
2023-11-12 23:14:27 +0100

    Small start

---
+++ netsio.c
@@ -0,0 +1,24 @@
+/*
+    Netsio in C to connect to Fujinet-PC
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+/* UDP network stuff */
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+/* Time for keepalive*/
+#include <sys/time.h>
+
+#include "netsio_proto.h"
+#include "netsio.h"
+
+int main(int argc, char** argv) {
+    
+    return 0;
+}
---
+++ netsio.h
@@ -0,0 +1,5 @@
+#ifndef NETSIO_H
+#define NETSIO_H
+
+
+#endif // NETSIO_H
