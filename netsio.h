43858f0dd3bce1fe028ce80181b99f69f8bb7c7e
Johan Tibbelin <42235680+johantibbelin@users.noreply.github.com>
2023-11-13 11:19:17 +0100

    Moved main to netsio_test.c

--- netsio.h
+++ netsio.h
@@ -1,5 +1,19 @@
 #ifndef NETSIO_H
 #define NETSIO_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+/* UDP network stuff */
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
+/* Time for keepalive*/
+#include <sys/time.h>
+
+#include "netsio_proto.h"
 
 #endif // NETSIO_H
--- netsio_test.c
+++ netsio_test.c
@@ -1,5 +1,10 @@
 /*
     Netsio in C to connect to Fujinet-PC
 */
+
 #include "netsio.h"
 
+int main(int argc, char** argv) {
+    
+    return 0;
+}
